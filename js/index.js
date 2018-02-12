0c3e194432b744091e0f44444b2c83910b128792
longhua.hu <longhua.hu@timanetworks.com>
2018-02-12 17:06:31 +0800

    写的什么乱七八糟的

---
+++ js/index.js
@@ -0,0 +1,20 @@
+$(function () {
+    getTime();
+    var timer = setInterval(()=>{
+        getTime();
+    },1000)
+})
+
+function getTime() {
+    var nowDate = new Date();
+    var year = nowDate.getFullYear();
+    var month = nowDate.getMonth() + 1 < 10 ? "0" + (nowDate.getMonth() + 1)
+        : nowDate.getMonth() + 1;
+    var day = nowDate.getDate() < 10 ? "0" + nowDate.getDate() : nowDate
+        .getDate();
+    var hours = nowDate.getHours()< 10 ? "0" + nowDate.getHours() : nowDate.getHours();
+    var minutes = nowDate.getMinutes()< 10 ? "0" + nowDate.getMinutes() : nowDate.getMinutes();
+    var seconds = nowDate.getSeconds()< 10 ? "0" + nowDate.getSeconds() : nowDate.getSeconds();
+    var dateStr = year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
+    $(".timer").val(dateStr);
+}
